e1566384514ce0eb0d9547b274b2709c123b7d5e
HarshalAtkar <harshalatkar@outlook.com>
2024-05-17 20:10:04 +0530

    added files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>My Portfolio </h1>
+        <p>Just started to build</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
